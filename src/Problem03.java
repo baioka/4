6e6c64673be13f2c40dffd7acadd0a84ae7a0ebd
baioka <bajarystanhb@gmail.com>
2026-05-26 22:21:36 +0500

    Update Problem03.java

--- src/Problem03.java
+++ src/Problem03.java
@@ -1,6 +1,15 @@
 public class Problem03 {
     public int findJudge(int n, int[][] trust) {
-        // write your solution here
+        int[] trustScores = new int[n + 1];
+        for (int[] relation : trust) {
+            trustScores[relation[0]]--;
+            trustScores[relation[1]]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (trustScores[i] == n - 1) {
+                return i;
+            }
+        }
         return -1;
     }
 }
