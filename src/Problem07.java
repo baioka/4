5140a029aa7d9ce1955b6a4111424d49175b9b87
baioka <bajarystanhb@gmail.com>
2026-05-26 22:23:05 +0500

    Update Problem07.java

--- src/Problem07.java
+++ src/Problem07.java
@@ -1,5 +1,50 @@
-public class Problem07 {
-    public boolean isBipartitionPossible(int n, int[][] dislikes) {
-        return false;
+import java.util.*;
+
+public class Problem06 {
+    public List<Integer> removeMethods(int n, int k, int[][] invocations) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] inv : invocations) {
+            adj.get(inv[0]).add(inv[1]);
+        }
+
+        Set<Integer> suspicious = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(k);
+        suspicious.add(k);
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            for (int neighbor : adj.get(curr)) {
+                if (!suspicious.contains(neighbor)) {
+                    suspicious.add(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        boolean canRemove = true;
+        for (int[] inv : invocations) {
+            if (!suspicious.contains(inv[0]) && suspicious.contains(inv[1])) {
+                canRemove = false;
+                break;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        if (canRemove) {
+            for (int i = 0; i < n; i++) {
+                if (!suspicious.contains(i)) {
+                    result.add(i);
+                }
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
