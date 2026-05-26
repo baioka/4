2f7eba6512fa78bb665f164f703fdc09d7ae3236
baioka <bajarystanhb@gmail.com>
2026-05-26 22:23:47 +0500

    Update Problem09.java

--- src/Problem09.java
+++ src/Problem09.java
@@ -1,6 +1,27 @@
+import java.util.Arrays;
+
 public class Problem09 {
     public int networkDelayTime(int[][] times, int n, int k) {
-        // write your solution here
-        return -1;
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k] = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int[] edge : times) {
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                if (dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                }
+            }
+        }
+
+        int maxDelay = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) return -1;
+            maxDelay = Math.max(maxDelay, dist[i]);
+        }
+        return maxDelay;
     }
 }
